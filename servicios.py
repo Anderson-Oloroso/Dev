392e9bba55bfcfea6148e56dd43df08e7bf700fb
Anderson-Oloroso <henrikog4@gmail.com>
2026-03-18 07:27:21 -0600

    :bug: Fix: Correction obçf bugs in function 'registrar_clientes'

--- servicios.py
+++ servicios.py
@@ -7,45 +7,54 @@ def registrar_clientes(data):
     try:
         dpi = int(input("DPI: "))
         nombre = input("Nombre: ")
-        riesgo = input("Riesgo (Alto, Medio, Bajo): ")
+        riesgo = input("Riesgo (Alto, Medio, Bajo): ").capitalize().strip()
     except Exception as e:
         print(f"Error de entrada: {e}")
         return
 
-    if riesgo not in riesgos:
-        print("Riesgo no válido. Por favor, ingrese Alto, Medio o Bajo.")
-    elif not nombre.strip():
-        print("Nombre no puede estar vacío.")
-    elif dpi != 13:
-        print("DPI debe tener 13 dígitos. Intente nuevamente.")
-    else:
-        print("Cliente registrado exitosamente.")
-
     try:
         cliente = {
-            "DPI": dpi,
+            "dpi": dpi,
             "Nombre": nombre,
             "Riesgo": riesgo
         }
         with open(data, "r") as file:
             clientes = json.load(file)
-
-        for doc in clientes:
-            print(doc['dpi'])
-            if dpi == doc['dpi']:
-                print("DPI ya registrado. No se puede registrar el cliente.")
-                break
             
         clientes.append(cliente)
     except Exception as e:
-        print(f"Error con el archivo: {e}")
+        print(f"Error: {e}")
 
-    try:
-        with open(data, "w") as f:
-            json.dump(clientes, f, indent=4)
-    except Exception as e:
-        print(f"Error con el archivo: {e}")
+    encontrado = False
 
-registrar_clientes(data)
+    for i, doc in enumerate(clientes):
+        docp = doc.get('dpi')
+        if dpi == docp:
+            encontrado = True
+
+    if encontrado == True:
+        print("El DPI que intenta registrar ya existe")
+        return
+    elif riesgo not in riesgos:
+        print("Riesgo no válido. Por favor, ingrese Alto, Medio o Bajo.")
+        return
+    elif not nombre.strip():
+        print("Nombre no puede estar vacío.")
+        return
+    elif len(str(dpi))!= 13:
+        print("DPI debe tener 13 dígitos. Intente nuevamente.")
+        return
+    else:
+        try:
+            with open(data, "w") as f:
+                json.dump(clientes, f, indent=4)
+                print("Cliente registrado exitosamente.")
+                
+        except Exception as e:
+            print(f"Error: {e}")
+
+#registrar_clientes(data)
+
+
 
 
